26d414d576f773c9268ae182c7db05fa50611756
Gianluca N Gozali <gianlucanicholasgozali@gmail.com>
2026-05-23 16:10:43 -0400

    Initial Commit

---
+++ PawMento/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PawMento
+//
+//  Created by max_ladmin on 5/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawMento/PawMentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawMentoApp.swift
+//  PawMento
+//
+//  Created by max_ladmin on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct PawMentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
